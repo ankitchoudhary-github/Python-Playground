125455cdcbf9840852827b70e6a835771201bac1
ankitchoudhary-github <itschoudharyankit@gmail.com>
2026-09-24 17:33:12 +0530

    Python Calculator

---
+++ Codes/5_PythonCalculator.py
@@ -0,0 +1,18 @@
+operator = input("Enter an Operator (+ - * /):")
+num1 = float(input ("Enter the first number: "))
+num2= float(input("Enter the second number:"))
+
+if operator =="+":
+    result = num1 + num2
+    print (round(result))
+elif operator =="-":
+    result = num1 - num2
+    print (round(result))
+elif operator == "*":
+    result = num1 * num2
+    print (round(result))
+elif operator == "/":
+    result = num1 / num2
+    print (round(result))
+else:
+    print(f"{operator} operator is not valid")
